bab89b7ff41605466c1bf2fe0e049f43b50b59ad
André Oliveira <castrodeko11@hotmail.com>
2022-06-18 22:56:39 -0300

    .\Refatoring Classes

--- src/main/java/br/com/aco/VendasApplication.java
+++ src/main/java/br/com/aco/VendasApplication.java
@@ -1,63 +1,12 @@
 package br.com.aco;
 
 
-import br.com.aco.domain.entity.Cliente;
-import br.com.aco.domain.entity.Pedido;
-import br.com.aco.domain.repository.Clientes;
-import br.com.aco.domain.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 
 @SpringBootApplication
 public class VendasApplication {
-
-    @Autowired
-    private Clientes clientes1;
-
-    @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos) {
-
-        return args -> {
-            System.out.println("Salvando clientes");
-            clientes.save(new Cliente("Dougllas"));
-            clientes.save(new Cliente("Outro Cliente"));
-
-            Cliente fulano = new Cliente("Andre");
-            clientes.save(fulano);
-
-            Pedido pedido = new Pedido();
-            pedido.setCliente(fulano);
-            pedido.setDataPedido(LocalDate.now());
-            pedido.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(pedido);
-
-//            boolean existe = clientes.existsByNome("Dougllas");
-//            System.out.println("Existe um cliente com o nome Dougllas: " + existe);
-
-//            List<Cliente> result = clientes.findByNomeLike("Dougllas");
-//            result.forEach(System.out::println);
-
-//
-//            Cliente clientePedidoFetch = clientes.findClienteFetchPedidos(fulano.getId());
-//            System.out.println(clientePedidoFetch);
-//            System.out.println(clientePedidoFetch.getPedidos());
-
-            pedidos.findByCliente(fulano).forEach(System.out::println);
-
-
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/br/com/aco/domain/entity/Cliente.java
+++ src/main/java/br/com/aco/domain/entity/Cliente.java
@@ -1,5 +1,7 @@
 package br.com.aco.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
@@ -10,8 +12,13 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(length = 100)
     private String nome;
 
+    @Column(length = 11)
+    private String cpf;
+
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private List<Pedido> pedidos;
 
@@ -49,6 +56,14 @@ public class Cliente {
 
     public void setPedidos(List<Pedido> pedidos) {
         this.pedidos = pedidos;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     @Override
